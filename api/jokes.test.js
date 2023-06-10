5e3c4d58b1e4d1493f3124f91fc33877c816af8d
Kevin MacFadyen <kmacfadyen3@gmail.com>
2023-06-10 18:49:18 -0400

    third

--- api/jokes.test.js
+++ api/jokes.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest')
 const db = require('../data/db-config')
 const server = require('../server')
+const Joke = require('./jokesModel')
 
 const joke1 = {
     joke: 'Why did the chicken cross the road',
@@ -28,3 +29,13 @@ it ('correct env variable', () => {
     expect(process.env.DB_ENV).toBe('testing')
 })
 
+describe('jokes model functions', () => {
+    describe('create joke', () => {
+        it('adds joke to database', async () => {
+            let jokes
+            await Joke.createJoke(joke1)
+            jokes = await db('jokes')
+            expect(jokes).toHaveLength(1)
+        })
+    })
+})
